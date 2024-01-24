ebedbdd746e942fcb9d2b0012af332ca9b87f429
Alan Omari <alan.omari@iths.se>
2024-01-24 10:02:59 +0100

    added new testing project,
    added the MSTest test framework, adapter and SDK to conduct unit tests, setup testing for car and motorbike, the implemented tests will check if we get the assumed correct toll fee value.

---
+++ Testing/Test.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class TollCalculatorTests
+    {
+        [TestMethod]
+        public void GetTollFee_WithCarAndValidDates_ShouldReturnCorrectFee()
+        {
+            var car = new TollCalculator.Vehicles.Car();
+            DateTime[] dates = new DateTime[]
+            {
+            new(2024, 1, 24, 6, 15, 0), 
+            new(2024, 1, 24, 8, 45, 0), 
+            new(2024, 1, 24, 15, 30, 0)
+            };
+
+            int totalTollFee = TollCalculator.TollCalculator.GetTollFee(car, dates);
+
+            
+            Assert.AreEqual(8 + 8 + 18, totalTollFee);
+        }
+
+        [TestMethod]
+        public void GetTollFee_WithMotorbikeAndValidDates_ShouldReturnCorrectFee()
+        {
+            var motorbike = new TollCalculator.Vehicles.Motorbike();
+            DateTime[] dates = new DateTime[]
+            {
+                new(2024, 1, 24, 6, 15, 0),
+                new(2024, 1, 24, 8, 45, 0),
+                new(2024, 1, 24, 15, 30, 0)
+            };
+
+            int totalTollFee = TollCalculator.TollCalculator.GetTollFee(motorbike, dates);
+
+            
+            Assert.AreEqual(8 + 8 + 18, totalTollFee);
+        }
+
+
+    }
+}
