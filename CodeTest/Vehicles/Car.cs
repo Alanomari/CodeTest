32feab2c42aa16d176dc65f0f3687bacd8df9184
Alan Omari <alan.omari@iths.se>
2024-01-24 08:59:15 +0100

    added new folder Vehicles in which we will store all our vehicle child classes.

---
+++ CodeTest/Vehicles/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TollFeeCalculator;
+
+namespace CodeTest.Vehicles
+{
+    public class Car : IVehicle
+    {
+        public VehicleType GetVehicleType()
+        {
+            return VehicleType.Car;
+        }
+    }
+}
